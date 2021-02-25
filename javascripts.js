c11ca383273afd023cfda1770f0d2f769f46c1bf
Andreas001 <andreasgeraldo0@gmail.com>
2021-02-25 10:16:01 +0700

    ok

--- javascripts.js
+++ javascripts.js
@@ -1,62 +1,86 @@
-// Collapse Navbar
-var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-    } else {
-        $("#mainNav").removeClass("navbar-shrink");
-    }
-};
-navbarCollapse();
-$(window).scroll(navbarCollapse);
+(function($) {
+    "use strict";
 
-// Load work.html first
-$(function (){
-    $("#page").load("aboutme.html");
-});
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: (target.offset().top - 71)
+            }, 1000, "easeInOutExpo");
+            return false;
+          }
+        }
+      });
 
-$(function(){
-    $("#work").click(function(){
-        $.ajax({
-            type: "get",
-            url: "work.html",
-            data: "",
-            dataType: "html",
-            beforeSend: function() {
-                $("#page").html("");
-            },
-            success: function (response) {
-                $("#page").html(response);
-            }
-        });
+    // Collapse Navbar
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    navbarCollapse();
+    $(window).scroll(navbarCollapse);
+
+    $(function (){
+        $("#page-about").load("aboutme.html");
     });
 
-    $("#about").click(function(){
-        $.ajax({
-            type: "get",
-            url: "aboutme.html",
-            data: "",
-            dataType: "html",
-            beforeSend: function() {
-                $("#page").html("");
-            },
-            success: function (response) {
-                $("#page").html(response);
-            }
-        });
+    $(function (){
+        $("#page-projects").load("projects.html");
     });
 
-    $("#cv").click(function(){
-        $.ajax({
-            type: "get",
-            url: "cv.html",
-            data: "",
-            dataType: "html",
-            beforeSend: function() {
-                $("#page").html("");
-            },
-            success: function (response) {
-                $("#page").html(response);
-            }
-        });
+    $(function (){
+        $("#page-cv").load("cv.html");
     });
-});
+
+    // $(function(){
+    //     $("#work").click(function(){
+    //         $.ajax({
+    //             type: "get",
+    //             url: "work.html",
+    //             data: "",
+    //             dataType: "html",
+    //             beforeSend: function() {
+    //                 $("#page").html("");
+    //             },
+    //             success: function (response) {
+    //                 $("#page").html(response);
+    //             }
+    //         });
+    //     });
+
+    //     $("#about").click(function(){
+    //         $.ajax({
+    //             type: "get",
+    //             url: "aboutme.html",
+    //             data: "",
+    //             dataType: "html",
+    //             beforeSend: function() {
+    //                 $("#page").html("");
+    //             },
+    //             success: function (response) {
+    //                 $("#page").html(response);
+    //             }
+    //         });
+    //     });
+
+    //     $("#cv").click(function(){
+    //         $.ajax({
+    //             type: "get",
+    //             url: "cv.html",
+    //             data: "",
+    //             dataType: "html",
+    //             beforeSend: function() {
+    //                 $("#page").html("");
+    //             },
+    //             success: function (response) {
+    //                 $("#page").html(response);
+    //             }
+    //         });
+    //     });
+    // });
+})(jQuery);
